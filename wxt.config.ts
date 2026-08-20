16c46af9ec3187282c9b3db716a49de5aea1b2df
Andrey Melchikov <andrei.melchikov25@gmail.com>
2026-08-21 00:31:20 +0400

    feat: migrate extension to WXT and React

---
+++ wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'wxt';
+
+export default defineConfig({
+  manifestVersion: 3,
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'Chrome Start Page',
+    version: '0.1.0',
+    description: 'Custom Chrome new tab page',
+  },
+});
